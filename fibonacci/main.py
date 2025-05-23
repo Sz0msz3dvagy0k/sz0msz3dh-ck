18b2229a317e895a13627ce3aed683425d6a77b0
Junghausz Benedek <junghauszbenedek@icloud.com>
2025-05-23 14:37:01 +0200

    fibonacci

--- fibonacci/main.py
+++ fibonacci/main.py
@@ -1,4 +1,23 @@
-with open('./input.txt', 'r') as f:
-  input = f.read()
+def fibonacci(n):
+  if not isinstance(n, int) or n < 0:
+    return "N/A"
+  
+  fib_sequence = []
+  a, b = 0, 1
+  while a <= n:
+    if a % 3 == 0:
+      fib_sequence.append(a)
+    a, b = b, a + b
+  
+  return ", ".join(map(str, fib_sequence)) if fib_sequence else "N/A"
 
-print(input)
+
+with open('./input.txt', 'r') as f:
+  inputs = f.read().splitlines()
+
+for line in inputs:
+  try:
+    n = int(line)
+    print(fibonacci(n))
+  except ValueError:
+    print("N/A")
